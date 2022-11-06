684273865daa57424f408059b8659e55ebbc0596
vladimir Demyanchuk <wovka.home@gmail.com>
2022-11-06 17:06:01 +0500

    правки env

--- src/render/telegram.tsx
+++ src/render/telegram.tsx
@@ -7,16 +7,22 @@ import { CommonBasketProvider } from '../store';
 
 dotenv.config();
 
+const { TELEGRAM_TOKEN, PORT } = process.env;
+
 const isDevelopment = process.env.NODE_ENV === 'development';
 
+if (!TELEGRAM_TOKEN) {
+    throw new Error('Provide TELEGRAM_TOKEN to .env https://core.telegram.org/bots#6-botfather');
+}
+
 const urbanBotTelegram = new UrbanBotTelegram({
-    token: '5649960131:AAGKpv8E5y2KMdrOfXSG8OCU14gS7J_eUpo',
+    token: TELEGRAM_TOKEN,
     isPolling: isDevelopment,
 });
 
 render(
     <CommonBasketProvider>
-        <Root bot={urbanBotTelegram} port={5000} sessionTimeSeconds={3600}>
+        <Root bot={urbanBotTelegram} port={PORT ? Number(PORT) : undefined} sessionTimeSeconds={3600}>
             <App />
         </Root>
     </CommonBasketProvider>,
