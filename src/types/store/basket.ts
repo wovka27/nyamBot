0f99f58db74db0df63c399c09ecf46a037fa8dbe
vladimir Demyanchuk <wovka.home@gmail.com>
2022-11-06 17:28:25 +0500

    правки

--- src/types/store/basket.ts
+++ src/types/store/basket.ts
@@ -8,51 +8,51 @@ export interface BasketItemType {
 }
 
 
-export type BasketItemsResponseDataType = {
-    msg: string;
-    code: number;
-    data: {
-        price: string;
-        basket: BasketItemType[];
-    };
-};
+// export type BasketItemsResponseDataType = {
+//     msg: string;
+//     code: number;
+//     data: {
+//         price: string;
+//         basket: BasketItemType[];
+//     };
+// };
+//
+// export type BasketListResponseDataType = {
+//     msg: string;
+//     code: number;
+//     data: {
+//         basket: {
+//             'stolovaya-sibinformbyuro': StolovayaSibinFormByuroDataType;
+//         };
+//         customer: '';
+//         stocks: StocksType[];
+//         payment_types: PaymentTypes;
+//         address: string;
+//     };
+// };
 
-export type BasketListResponseDataType = {
-    msg: string;
-    code: number;
-    data: {
-        basket: {
-            'stolovaya-sibinformbyuro': StolovayaSibinFormByuroDataType;
-        };
-        customer: '';
-        stocks: StocksType[];
-        payment_types: PaymentTypes;
-        address: string;
-    };
-};
+// export type StocksType = {
+//     type: {
+//         type: null;
+//     };
+//     products: BasketItemType[];
+//     producer_pk: number;
+//     producer_slug: string;
+// };
+//
+// export type PaymentTypes = {
+//     courier_cash: string;
+//     courier_card: string;
+//     card_online: string;
+// };
 
-export type StocksType = {
-    type: {
-        type: null;
-    };
-    products: BasketItemType[];
-    producer_pk: number;
-    producer_slug: string;
-};
-
-export type PaymentTypes = {
-    courier_cash: string;
-    courier_card: string;
-    card_online: string;
-};
-
-export type StolovayaSibinFormByuroDataType = {
-    delivery_min_price: number;
-    title: string;
-    delivery_price: number;
-    open_time_if_closed: unknown | null;
-    products: BasketItemType[];
-    payment_types: ['courier_cash', 'card_online'];
-    link: string;
-    open_time: unknown | null;
-};
+// export type StolovayaSibinFormByuroDataType = {
+//     delivery_min_price: number;
+//     title: string;
+//     delivery_price: number;
+//     open_time_if_closed: unknown | null;
+//     products: BasketItemType[];
+//     payment_types: ['courier_cash', 'card_online'];
+//     link: string;
+//     open_time: unknown | null;
+// };
